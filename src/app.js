0ba8afe595376c3b7a1b80e905a54812ab53dbf5
Nattnun <142487291+Nattnun@users.noreply.github.com>
2024-02-07 17:46:21 +0700

    end of the day

--- src/app.js
+++ src/app.js
@@ -5,17 +5,21 @@ const morgan = require("morgan");
 const errorMiddleware = require("./middlewares/error");
 const notFoundMiddleware = require("./middlewares/not-found");
 const authRoute = require("./routes/auth-route");
+const userRoute = require("./routes/user-route");
 
 const limiter = require("./middlewares/rate-limit");
+const authenticate = require("./middlewares/authenticate");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json()); //convert data to json object
 app.use(limiter); // limit amount of request
-app.use(morgan("combined")); //morgan will log time and method of request and response
+app.use(morgan("dev")); //morgan will log time and method of request and response
+app.use("/public", express.static("public"));
 
 app.use("/auth", authRoute);
+app.use("/users", authenticate, userRoute);
 
 // notfound and error handler
 app.use(notFoundMiddleware);
---
+++ src/routes/user-route.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const upload = require("../middlewares/upload");
+const userController = require("../controllers/user-controller");
+
+const router = express.Router();
+
+router.patch(
+  "/",
+  upload.fields([
+    { name: "profileImage", maxCount: 1 },
+    { name: "coverImage", maxCount: 1 },
+  ]),
+  userController.updateUser
+);
+
+module.exports = router;
